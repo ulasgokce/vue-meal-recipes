ed0aadb24422ee3d98bf92171b7ad724c4c30f4b
ulasgokce <ulasgokce98@gmail.com>
2024-05-07 00:15:57 +0300

    composition api testing app

---
+++ README.md
@@ -0,0 +1,5 @@
+## Installation
+1. Clone the repository
+1. Run `npm install`
+1. Copy `.env.example` into `.env`
+1. Run `npm run dev` to start the application at http://localhost:3000
---
+++ src/components/YouTubeButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <a
+    :href="href"
+    target="_blank"
+    class="px-3 py-2 rounded border-2 text-white border-cyan-600 bg-cyan-500 hover:bg-cyan-600 transition-colors"
+  >
+    <slot>YouTube</slot>
+  </a>
+</template>
+
+<script setup>
+const props = defineProps({
+  href: String
+})
+</script>
